7658c1293d0aa6dbfe4471ed0b8a43ef0d1b07bd
ro0ti <81922230+ro0ti@users.noreply.github.com>
2023-05-18 00:49:14 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
 // Read page to understand which helper to load.
-const Page = document.getElementById("header-title");
-const Pages = [];
+var TargetPage = document.getElementById("header-title");
+
+Init();
+setInterval(function() {
+    //console.clear();
+    //console.log("Melvor Idle Bot - v1.0 [ro0ti#2266]\nPage Loaded: " + TargetPage.innerText + "\nEvent Loaded: " + TargetPage.innerText + ".js");
+    Init();
+}, 5000);
 
 function Init() {
-    if (Page) {
-        if (!Pages.includes(Page)) { Pages.push(Page); } // Handles each page.
-        HelperAPI(Page);
-    } else {
-        var isGame = setInterval(function() { // If not found, load until it catches it.
-            Init();
-            clearInterval(isGame);
-        }, 1000);
+    if (TargetPage) {
+        HelperAPI(TargetPage.innerHTML);
     }
 }
 
 function HelperAPI(event) {
-    const api = `https://raw.githubusercontent.com/ro0ti/Melvor-Idle-Bot-v1.0/main/events/${event}.js?token=GHSAT0AAAAAACCOI5XG3RAPPVS4U35S56WIZDFUVAQ`;
-    const request = new XMLHttpRequest();
+    var api = `https://raw.githubusercontent.com/ro0ti/Melvor-Idle-Bot-v1.0/main/events/${event}.js?token=GHSAT0AAAAAACCOI5XG3RAPPVS4U35S56WIZDFUVAQ`;
+    var request = new XMLHttpRequest();
     request.onload = function(err) {
-        if (err) { HelperHeader(err); }
-        const func = new Function(`${this.responseText}`);
+        if (err) { console.log(err); }
+        var func = new Function(`${this.responseText}`);
         func();
-        HelperHeader("Event Loaded");
     }
     request.open("GET", api, true);
     request.send();
-}
-
-function HelperHeader(message) {
-    Page.innerText = Page.innerText + ' - ' + message; 
 }
