b0dac88c2748f1ce528f27a76d89a27845209780
ro0ti <81922230+ro0ti@users.noreply.github.com>
2023-05-18 13:05:06 -0400

    Update Combat.js

--- events/Combat.js
+++ events/Combat.js
@@ -1,1 +1,23 @@
 // Combat
+const HealthBar = document.getElementById("combat-player-hitpoints-bar");
+
+Script();
+
+function Script() {
+    if (HealthBar) {
+        var rawValue = HealthBar.style.width;
+        var HealthValue = Math.floor(rawValue.replace("%", ""));
+
+        if (HealthValue <= 70) {
+            var HealButtons = document.getElementsByClassName("btn text-combat-smoke font-size-sm btn-outline-secondary");
+            for (var i = 0; i < HealButtons.length; i++) {
+                HealButtons[i].click();
+            }
+            alert("Auto healed.");
+        }
+    } else {
+        setInterval(function() {
+            Script();
+        }, 1000);
+    }
+}
