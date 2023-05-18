e5228f87ca7a910af591058b1c6224c29782787d
ro0ti <81922230+ro0ti@users.noreply.github.com>
2023-05-18 00:09:09 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Melvor-Idle-Bot-v1.0
- Exactly what it is, Instead of cheating and make it boring use a bot to be more helpful.
+Exactly what it is, Instead of cheating and make it boring use a bot to be more helpful.
+
+# Early State
+This is a very early state project, There will be updated when there is updated sorry.
---
+++ app.js
@@ -0,0 +1,32 @@
+// Read page to understand which helper to load.
+const Page = document.getElementById("header-title");
+const Pages = [];
+
+function Init() {
+    if (Page) {
+        if (!Pages.includes(Page)) { Pages.push(Page); } // Handles each page.
+        HelperAPI(Page);
+    } else {
+        var isGame = setInterval(function() { // If not found, load until it catches it.
+            Init();
+            clearInterval(isGame);
+        }, 1000);
+    }
+}
+
+function HelperAPI(event) {
+    const api = `https://raw.githubusercontent.com/ro0ti/Melvor-Idle-Bot-v1.0/main/events/${event}.js`;
+    const request = new XMLHttpRequest();
+    request.onload = function(err) {
+        if (err) { HelperHeader(err); }
+        const func = new Function(`${this.responseText}`);
+        func();
+        HelperHeader("Event Loaded");
+    }
+    request.open("GET", api, true);
+    request.send();
+}
+
+function HelperHeader(message) {
+    Page.innerText = Page.innerText + ' - ' + message; 
+}
